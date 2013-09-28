dcc38041d507ac3ede1beda085b7d1e08b3afbc0
Eric Dobson <eric.n.dobson@gmail.com>
2013-09-28 16:43:20 -0400

    Move function combinators to its own file.

--- combinators.rkt
+++ combinators.rkt
@@ -1,23 +1,14 @@
 #lang racket/base
 
-(require "structures.rkt" racket/list
-         racket/match
-         (for-syntax racket/base racket/syntax syntax/parse))
-
-;; for syntax
-(require racket/set
+(require "structures.rkt" "function-combinator.rkt"
+         racket/list racket/match
+         (for-syntax racket/base racket/syntax syntax/parse)
+         racket/set
          unstable/contract
          (except-in racket/contract recursive-contract))
 
-(provide (except-out (all-defined-out) function/sc)
-  (contract-out
-    [function/sc (-> (listof static-contract?)
-                     (listof static-contract?)
-                     (listof (list/c keyword? static-contract?))
-                     (listof (list/c keyword? static-contract?))
-                     (or/c #f static-contract?)
-                     (or/c #f (listof static-contract?))
-                     static-contract?)]))
+(provide (all-defined-out)
+         (all-from-out "function-combinator.rkt"))
 
 
 ;;Printing
@@ -134,12 +125,6 @@
 (struct continuation-mark-key-combinator chaperone-combinator ())
 (struct prompt-tag-combinator chaperone-combinator ())
 (struct parametric-combinator flat-combinator ())
-(struct function-combinator chaperone-combinator (indices mand-kws opt-kws)
-        #:property prop:combinator-name "->/sc"
-        #:methods gen:equal+hash [(define (equal-proc a b recur) (function-sc-equal? a b recur))
-                                  (define (hash-proc v recur) (function-sc-hash v recur))
-                                  (define (hash2-proc v recur) (function-sc-hash2 v recur))]
-        #:methods gen:sc-mapable [(define (sc-map v f) (function-sc-map v f))])
 (struct case->-combinator chaperone-combinator ())
 (struct arr-combinator chaperone-combinator ())
 
@@ -165,116 +150,6 @@
 (define any/sc (any-combinator #'any/c 'flat))
 (define identifier/sc (flat/sc #'identifier?))
 
-
-
-(define (split-function-args ctcs mand-args-end opt-args-end
-                    mand-kw-args-end opt-kw-args-end rest-end range-end)
-  (values
-    (drop (take ctcs mand-args-end) 0)
-    (drop (take ctcs opt-args-end) mand-args-end)
-    (drop (take ctcs mand-kw-args-end) opt-args-end)
-    (drop (take ctcs opt-kw-args-end) mand-kw-args-end)
-    (and (> rest-end opt-kw-args-end)
-         (first (drop (take ctcs rest-end) opt-kw-args-end)))
-    (and range-end (drop (take ctcs range-end) rest-end))))
-
-(define (function/sc mand-args opt-args mand-kw-args opt-kw-args rest range)
-  (define mand-args-end (length mand-args))
-  (define opt-args-end (+ mand-args-end (length opt-args)))
-  (define mand-kw-args-end (+ opt-args-end (length mand-kw-args)))
-  (define opt-kw-args-end (+ mand-kw-args-end (length opt-kw-args)))
-  (define rest-end (if rest (add1 opt-kw-args-end) opt-kw-args-end))
-  (define range-end (and range (+ rest-end (length range))))
-  (define mand-kws (map first mand-kw-args))
-  (define opt-kws (map first opt-kw-args))
-  (define end-indices
-    (list mand-args-end opt-args-end mand-kw-args-end opt-kw-args-end rest-end range-end))
-
-  (function-combinator
-    (lambda ctcs
-      (define-values (mand-ctcs opt-ctcs mand-kw-ctcs opt-kw-ctcs rest-ctc range-ctcs)
-        (split-function-args ctcs end-indices))
-
-      (define mand-kws-stx (append-map list mand-kws mand-kw-ctcs))
-      (define opt-kws-stx (append-map list opt-kws opt-kw-ctcs))
-      (define rest-ctc-stx
-        (if rest-ctc
-          (list '#:rest rest-ctc)
-          #'()))
-
-      (define range-ctc
-        (if range-ctcs
-          #`(values #,@range-ctcs)
-          #'any))
-
-
-      #`((#,@mand-ctcs #,@mand-kws-stx)
-         (#,@opt-ctcs #,@opt-kws-stx) 
-         #,@rest-ctc-stx
-         . ->* . #,range-ctc))
-    (append
-      mand-args
-      opt-args
-      (map second mand-kw-args)
-      (map second opt-kw-args)
-      (if rest (list rest) null)
-      (or range null))
-    end-indices
-    mand-kws
-    opt-kws))
-
-
-(define-match-expander ->/sc:
-  (syntax-parser
-    [(_ mand-args opt-args mand-kw-args opt-kw-args rest range)
-     #'(and (? function-combinator?)
-            (app (match-lambda
-                   [(function-combinator stx args indices mand-kws opt-kws)
-                    (define-values (mand-args* opt-args* mand-kw-args* opt-kw-args* rest* range*)
-                      (apply split-function-args args indices))
-                    (list
-                      mand-args* opt-args*
-                      (map list mand-kws mand-kw-args*)
-                      (map list opt-kws opt-kw-args*)
-                      rest*
-                      range*)])
-                 (list mand-args opt-args mand-kw-args opt-kw-args rest range)))]))
-
-
-(define (function-sc-map v f)
-  (match-define (function-combinator stx args indices mand-kws opt-kws) v) 
-
-  (define-values (mand-args opt-args mand-kw-args opt-kw-args rest-arg range-args)
-    (apply split-function-args args indices))
-
-  (define new-args
-    (append 
-      (map (lambda (arg) (f arg 'contravariant))
-           (append mand-args opt-args mand-kw-args opt-kw-args (if rest-arg (list rest-arg) null)))
-      (if range-args
-          (map (lambda (arg) (f arg 'covariant))
-               range-args)
-          empty)))
-        
-
-  (function-combinator stx new-args indices mand-kws opt-kws))
-
-(define (function-sc-equal? a b recur)
-  (match-define (function-combinator a-stx a-args a-indices a-mand-kws a-opt-kws) a) 
-  (match-define (function-combinator b-stx b-args b-indices b-mand-kws b-opt-kws) b) 
-  (and
-    (recur a-indices b-indices)
-    (recur a-mand-kws b-mand-kws)
-    (recur a-opt-kws b-opt-kws)
-    (recur a-args b-args)))
-
-(define (function-sc-hash v recur)
-  (match-define (function-combinator a-stx v-args v-indices v-mand-kws v-opt-kws) v) 
-  (+ (recur v-indices) (recur v-mand-kws) (recur v-opt-kws) (recur v-args)))
-
-(define (function-sc-hash2 v recur)
-  (match-define (function-combinator a-stx v-args v-indices v-mand-kws v-opt-kws) v) 
-  (+ (recur v-indices) (recur v-mand-kws) (recur v-opt-kws) (recur v-args)))
 
 
 (define case->/sc 
@@ -309,7 +184,6 @@
 
 (define (parametric->/sc vars body)
   (parametric-combinator (λ (ctc) #`(parametric->/c (#,@vars) #,ctc)) body))
-
 
 
 (define-match-expander any/sc:
